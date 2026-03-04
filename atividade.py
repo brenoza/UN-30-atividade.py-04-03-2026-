7f931663b5f41d002035525eef14f217334f7a4c
brenoza <brenojustino669@gmail.com>
2026-03-04 15:49:33 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,10 @@
+
+compras = ["arroz", "feijão", "macarrão", "leite", "pão"]
+
+print("Lista de compras:", compras)
+
+novo_item = input("escreva um item para botar na lista de compras: ")
+
+compras.append(novo item)
+
+print("Lista Atualizada:", compras)
---
+++ listas2.py
@@ -0,0 +1,29 @@
+animal = ["Cachorro", "Gato"]
+print("Lista inicial:", animal)
+
+animal.append("Pato")
+print("Lista atualizado: ", animal)
+
+animal.insert(1, "Coelho")
+
+print("Lista atualizado: ",animal)
+
+animal.extend(["macaco", "leão"])
+print("Listar atualizado:", animal)
+
+animal.remove("leão")
+print(animal)
+
+removido = animal.pop()
+print(f"Removido: {removido}")
+print("Após pop()", animal)
+
+removido2 = animal.pop(1)
+print(f"Removido do índice 1 {removido2}")
+print("Após pop(1):", animal)
+
+del animal[0]
+print("Apos o del", animal)
+
+animal.clear()
+print(animal)
